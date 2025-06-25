701b2e01dc09dc63f65fa10e66a347a70c49a0ba
eduardoronsoni <eduardoronsoni10@gmail.com>
2025-06-24 21:48:09 -0300

    adding model version

---
+++ models/stg/stg_ordinary__customers_v2.sql
@@ -0,0 +1,25 @@
+/* This overwrites the materialization config passed on dbt_project */
+{{ config(
+    materialized='table' 
+) }}
+
+with source_data as (
+    select 
+        customer_id
+        , name as customer_name
+        , email as customer_email
+        , cast(created_at as date) as created_at
+        , cast(updated_at as timestamp) as updated_at
+        , cast(loaded_at as timestamp) as loaded_in
+    from {{ source('ordinary_shop', 'customers') }}
+)
+
+, generate_sk as (
+    select 
+        {{ dbt_utils.generate_surrogate_key(['customer_id']) }} as customer_sk
+        , *
+    from source_data
+)
+
+select *
+from generate_sk
