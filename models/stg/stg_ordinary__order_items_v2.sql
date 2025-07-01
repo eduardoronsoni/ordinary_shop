73db067f426d33a99333d94c4ed49d9cd4c2c766
eduardoronsoni <eduardoronsoni10@gmail.com>
2025-07-01 19:17:04 -0300

    adding v2 model and yaml to stg_ordinary_customer

---
+++ models/stg/stg_ordinary__order_items_v2.sql
@@ -0,0 +1,21 @@
+with source_data as (
+    select 
+        order_id
+        , product_id
+        , cast(quantity as int) as quantity
+        , cast(unit_price as float64) as unit_price
+        , cast(created_at as date) as created_at
+        , cast(updated_at as timestamp) as updated_at
+        , cast(loaded_at as timestamp) as loaded_at
+    from {{ source('ordinary_shop', 'order_items')}}
+)
+
+,  generate_sk as (
+    select 
+        {{ dbt_utils.generate_surrogate_key(['order_id','product_id']) }} as order_item_sk
+        , *
+    from source_data
+)
+
+select *
+from generate_sk
